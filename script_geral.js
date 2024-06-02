85897e2ece5d1c3c58e25d323e4f97df6ad45acd
Thiago Antonio <115923226+thiago-antonio@users.noreply.github.com>
2024-06-02 00:26:04 -0300

    parte visual do sistema

---
+++ script_geral.js
@@ -0,0 +1,16 @@
+const btn_menuPrincipal = document.querySelector("#btn_menuPrincipal")
+const menuPrincipal = document.querySelector("#menuPrincipal")
+const todosmenusprincipais = [...document.querySelectorAll(".btn_menuItem")]
+
+btn_menuPrincipal.addEventListener("click",(evt)=>{
+    menuPrincipal.classList.toggle("ocultar")
+})
+
+
+todosmenusprincipais.forEach(e=>{
+    e.addEventListener("click",(evt)=>{
+        menuPrincipal.classList.add("ocultar")   
+    })
+})
+
+
