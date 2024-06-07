0b0533107b2f1e9c3df7169d0c2ea839a174b96e
Thiago Antonio <115923226+thiago-antonio@users.noreply.github.com>
2024-06-06 23:26:56 -0300

    gravando telefonesdo colaborador

--- backoffice/colaboradores/colaboradores.js
+++ backoffice/colaboradores/colaboradores.js
@@ -4,7 +4,11 @@ const novoColaborador = document.querySelector("#novoColaborador")
 const btn_fecharPopup = document.querySelector("#btn_fecharPopup")
 const btn_gravarPopup = document.querySelector("#btn_gravarPopup")
 const btn_cancelarPopup = document.querySelector("#btn_cancelarPopup")
+const telefones = document.querySelector("#telefones")
+const f_fone = document.querySelector("#f_fone")
+const f_nome = document.querySelector("#f_nome")
 const f_tipoColab = document.querySelector("#f_tipoColab")
+const f_status = document.querySelector("#f_status")
 
 const endpoint_todoscolabodores = `http://127.0.0.1:1880/todosusuarios`
 fetch(endpoint_todoscolabodores)
@@ -66,8 +70,44 @@ btn_fecharPopup.addEventListener("click",(evt)=>{
 })
 
 btn_gravarPopup.addEventListener("click",(evt)=>{
-  novoColaborador.classList.add("ocultarPopup")
+  const tels = [...document.querySelectorAll(".nunTel")]
+  let nunTels = []
+  tels.forEach(t=>{
+    nunTels.push(t.innerHTML)
+  })
+  const dados = {
+    s_nome_usuario: f_nome.value,
+    n_tipousuario_tipousuario: f_tipoColab.value,
+    c_status_usuario: f_status.value,
+    nuntelefones:nunTels  
+  }
+  console.log(dados)
+  //novoColaborador.classList.add("ocultarPopup")
 })
 btn_cancelarPopup.addEventListener("click",(evt)=>{
   novoColaborador.classList.add("ocultarPopup")
+})
+
+f_fone.addEventListener("keyup",(evt)=>{
+  if(evt.key=="Enter"){
+    const divTel = document.createElement("div")
+    divTel.setAttribute("class","tel")
+
+    const nunTel = document.createElement("div")
+    nunTel.setAttribute("class","nunTel")
+    nunTel.innerHTML = evt.target.value
+    divTel.appendChild(nunTel)
+
+    const delTel = document.createElement("img")
+    delTel.setAttribute("src","../../imgs/delete.svg")
+    delTel.setAttribute("class","delTel")
+    delTel.addEventListener("click",(evt)=>{
+      evt.target.parentNode.remove()
+    })
+    divTel.appendChild(delTel)
+
+    telefones.appendChild(divTel)
+    evt.target.value = ""
+  }
+  
 })
